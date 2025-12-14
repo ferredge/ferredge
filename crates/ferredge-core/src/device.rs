f4166e0c3bfb686347f2051df140b40433db874a
Souyama <souyamadebnath@gmail.com>
2025-12-14 19:49:52 +0530

    Implement ProtocolBridge trait and enhance DeviceProtocol and DeviceEndpoint enums

--- crates/ferredge-core/src/device.rs
+++ crates/ferredge-core/src/device.rs
@@ -19,13 +19,41 @@ pub enum DeviceStatus {
     Unknown,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum DeviceProtocol {
+    MQTT,
+    HTTP,
+    Modbus,
+    CoAP,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum DeviceEndpoint {
+    Http { url: String, port: u16 },
+    Mqtt { broker: String, topic: String },
+    Modbus { address: u8, port: u16 },
+    CoAP { url: String, port: u16 },
+}
+
+impl DeviceEndpoint  {
+    fn protocol(&self) -> DeviceProtocol {
+        match self {
+            DeviceEndpoint::Http { .. } => DeviceProtocol::HTTP,
+            DeviceEndpoint::Mqtt { .. } => DeviceProtocol::MQTT,
+            DeviceEndpoint::Modbus { .. } => DeviceProtocol::Modbus,
+            DeviceEndpoint::CoAP { .. } => DeviceProtocol::CoAP,
+        }
+    }
+}
+
 /// Represents the metadata and state of a connected device.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Device {
     pub id: DeviceId,
     pub name: String,
-    pub protocol: String,
+    pub protocol: DeviceProtocol,
     pub status: DeviceStatus,
+    pub endpoint: DeviceEndpoint,
     // Additional metadata can be stored as a generic map or specific struct
     // depending on future needs.
     pub metadata: Option<Map<String, String>>,
@@ -35,13 +63,15 @@ impl Device {
     pub fn new(
         id: impl Into<String>,
         name: impl Into<String>,
-        protocol: impl Into<String>,
+        endpoint: impl Into<DeviceEndpoint>,
         metadata: Option<Map<String, String>>,
     ) -> Self {
+        let endpoint = endpoint.into();
         Self {
             id: id.into(),
             name: name.into(),
-            protocol: protocol.into(),
+            protocol: endpoint.protocol(),
+            endpoint: endpoint.clone(),
             status: DeviceStatus::Unknown,
             metadata: metadata,
         }
