f15f5457c097da43999cb08ba1133f30f3ca0c6c
Souyama <souyamadebnath@gmail.com>
2026-04-15 23:41:00 +0530

    feature gate core

--- crates/ferredge-core/src/lib.rs
+++ crates/ferredge-core/src/lib.rs
@@ -1,19 +1,25 @@
 mod bridge;
 mod command;
 mod device;
+#[cfg(feature = "net")]
 mod net;
 mod routed;
 mod router;
+#[cfg(feature = "runtime")]
 mod runtime;
+#[cfg(feature = "sync")]
 mod sync;
 
 pub mod prelude {
     pub use crate::bridge::*;
     pub use crate::command::*;
     pub use crate::device::*;
+    #[cfg(feature = "net")]
     pub use crate::net::*;
     pub use crate::routed::*;
     pub use crate::router::*;
+    #[cfg(feature = "runtime")]
     pub use crate::runtime::*;
+    #[cfg(feature = "sync")]
     pub use crate::sync::*;
 }
